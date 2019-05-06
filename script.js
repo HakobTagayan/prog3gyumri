8151719a647a91e1d5c5efe345d32c8a7666dcfa
Your Name <you@example.com>
2019-05-06 15:26:13 +0400

    tarberak dasi hamar

--- script.js
+++ script.js
@@ -1,38 +1,43 @@
-// var obj = {
-//     "first_name": "Vardan",
-//     "last_name": "Hovsepyan",
-//     "age": 13,
-//     "tumo_student": true,
-//     sayHello() {
-//         console.log("Hello");
-//     }
- 
-//  }
-//  console.log(obj);
-//  console.log(obj.first_name);
-//  obj.sayHello();
+// operation system
 // var os = require("os");
 // var message = "The platform is ";
 
 // function main(){
 //    console.log(message + os.platform());
 // }
+
 // main();
-// var express = require("express");
-// var app = express();
+
+//MODULE EXPRESS WICH RETURN A FUNCTION AND HAVE METHODS LIKE GET POST PUT DELETE
+
+var express = require("express");
+var app = express();
+
 
 // app.get("/", function(req, res){
-//    res.send("<h1>Hello world</h1>");
+//    res.send("Hello world");
+// });
+
+// app.listen(3000, function(){
+//    console.log("Example is running on port 3000");
+// });
+
+
+// app.get("/", function(req, res){
+//    res.send("<h1>Hello Gyumri</h1>");
 // });
 // app.get("/hakob", function(req, res){
 //     res.send("<h1>Hello hakob</h1>");
 //  });
-// app.get("/name/:name", function(req, res){
-//    var name = req.params.name;
-//    res.send("<h1>Hello " + name +"</h1>");
-// });
+//  app.get("/name/:name", function(req, res){
+//     var name = req.params.name;
+//     res.send("<h1>Hello " + name +"</h1>");
+//  });
+ 
 
 //stex sksvuma google link dnely
+
+
 // app.get("/hhh/:poxos", function(req, res){
 //     res.redirect('http://google.com');
 //  });
@@ -41,7 +46,7 @@
 //     res.redirect('http://google.com/search?q=' + val);
 //  });
 //  app.get("/*/", function(req, res){
-//     res.send("<h1>Hello " +404+"</h1>");
+//     res.send("<h1>Error " + 404 + "</h1>");
 // });
 // app.listen(3000, function(){
 //    console.log("Example is running on port 3000");
@@ -52,10 +57,10 @@
 // var express = require("express");
 // var app = express();
 
-// app.use(express.static("ProjectGOTL"));
+// app.use(express.static("Inharitance_Prog3"));
 
-// app.get("/", function(req, res){
-//    res.redirect("ProjectGOTL");
+// app.get("/hakob", function(req, res){
+//    res.redirect("Inharitance_Prog3");
 // });
 
 // app.listen(3000, function(){
